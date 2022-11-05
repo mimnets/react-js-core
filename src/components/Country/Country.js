fdec596ee6227298435c5d80ac3e7c81f14099d9
Monirul Islam <mdmonirulislammonir@gmail.com>
2022-11-05 23:56:25 +0600

    added bootstrap and modified background

--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -4,7 +4,7 @@ import './Country.css';
 const Country = (props) => {
     const {name, flags} = props.country;
     return (
-        <div className='country'>
+        <div className='country bg-warning'>
             <h2>{name.common}</h2>
             <img src={flags.png} alt=""/>
         </div>
